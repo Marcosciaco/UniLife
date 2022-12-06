581c87f9e1915a1b88e7f001d54562000b33c01d
Marco Sciacovelli <36106337+Marcosciaco@users.noreply.github.com>
2022-12-06 15:50:01 +0100

    Updated styling of Profile page

--- views/Profile.tsx
+++ views/Profile.tsx
@@ -1,98 +1,85 @@
 import React from "react";
-import { View, Text, StyleSheet, ImageBackground,Pressable} from "react-native";
+import {
+    View,
+    Text,
+    StyleSheet,
+    ImageBackground,
+    Pressable,
+} from "react-native";
 import { useState } from "react";
-const imgProfile = { uri: "https://www.nelsalento.com/wp-content/uploads/2018/02/Grotta-Verde-di-Andrano1.jpg.webp" };
+import { Poppins_400Regular } from "@expo-google-fonts/poppins";
+const imgProfile = {
+    uri: "https://www.nelsalento.com/wp-content/uploads/2018/02/Grotta-Verde-di-Andrano1.jpg.webp",
+};
 
-
-export function FollowButton(props:any) {
-
+export function FollowButton(props: any) {
     const { onPress } = props;
     const [following, setFollowing] = useState<boolean>(true);
-    const buttonText = following?"Unfollow":"Follow"
+    const buttonText = following ? "Unfollow" : "Follow";
 
     const toggle = () => {
-      onPress()
-      setFollowing(!following)
-    }
-    //const [title1, setButtonText] = useState('Follow');
-    //if(title1 == 'Follow')
-    //    setButtonText('Unfollow')
-    //else if (title1 == 'Unfollow')
-    //    setButtonText('Follow')
-    //else ;
+        onPress();
+        setFollowing(!following);
+    };
     return (
-      <Pressable style={styles.button} onPress={toggle}>
-        <Text style={styles.text}>{buttonText}</Text>
-      </Pressable>
+        <Pressable style={styles.button} onPress={toggle}>
+            <Text style={styles.text}>{buttonText}</Text>
+        </Pressable>
     );
-  }
+}
 
 export default function ProfileScreen({ navigation }: any) {
     return (
         <View style={styles.container}>
+            <ImageBackground
+                style={styles.logo}
+                source={imgProfile}
+                resizeMode="cover"
+            >
+                <Text style={styles.title}>Marco Sciacovelli</Text>
+                <Text style={styles.subtitle}>
+                    Bolzano Italy / Computer Science 3rd year{" "}
+                </Text>
+                <FollowButton onPress={() => null} />
+            </ImageBackground>
+        </View>
+    );
+}
 
-        {/* resizeMode (cover):
-        
-        contain : Scale the image uniformly (maintain the image's aspect ratio)
-        so that both dimensions (width and height) of the image will be equal to
-        or less than the corresponding dimension of the view (minus padding).
-        stretch : Scale width and height independently,
-        This may change the aspect ratio of the src. */}
-
-        <ImageBackground style = {styles.logo} source={imgProfile} resizeMode="cover">
-        <Text style={styles.title}>User1</Text>
-        <Text style={styles.subtitle}>Bolzano Italy / Computer Science 3rd year </Text>
-        <FollowButton onPress={() => null}/>
-        </ImageBackground>
-        </View>    
-        );
-    }
-    
-    export const styles = StyleSheet.create({
-        container: {
-            flex: 1,
-            backgroundColor: "#fff",
-        },
-        button: {
-            alignItems: 'center',
-            justifyContent: 'center',
-            paddingVertical: 20,
-            paddingHorizontal: 50,
-            borderRadius: 4,
-            elevation: 3,
-            backgroundColor: 'grey',
-            marginTop:65,
-            marginLeft:10,
-            marginRight:10,
-            marginEnd:10,
-          },
-          text: {
-            fontSize: 16,
-            lineHeight: 21,
-            fontWeight: 'bold',
-            letterSpacing: 0.25,
-            color:"lightblue",
-          },
-        title: {
-            color:"lightblue",
-            fontSize: 42,
-            fontWeight: "bold",
-            textAlign: "left",
-            marginTop: 500,
-            backgroundColor: "#000000c",
-            font:"poppins"
-        },
-        subtitle: {
-            color:"white",
-            fontSize:21,
-            fontWeight: "normal",
-            textAlign: "left",
-            marginTop: 5,
-            backgroundColor: "#000000c",
-            font:"poppins"
-        },
-        logo: {
-            flex: 1,
-            justifyContent: "center"
-        },
-    });
+export const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: "#fff",
+    },
+    button: {
+        alignItems: "center",
+        justifyContent: "center",
+        borderRadius: 10,
+        height: 66,
+        margin: 20,
+        elevation: 3,
+        backgroundColor: "#2B363F",
+        fontFamily: "Poppins_400Regular",
+    },
+    text: {
+        fontSize: 24,
+        fontFamily: "Poppins_400Regular",
+        color: "#AECFFF",
+    },
+    title: {
+        fontFamily: "Poppins_400Regular",
+        color: "#AECFFF",
+        fontSize: 24,
+        marginLeft: 20,
+    },
+    subtitle: {
+        color: "#F3F8FF",
+        fontSize: 13,
+        fontFamily: "Poppins_400Regular",
+        marginLeft: 20,
+    },
+    logo: {
+        flex: 1,
+        justifyContent: "flex-end",
+    },
+});
